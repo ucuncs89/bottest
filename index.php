3844d51307cf3b221144307467adca527225cc6c
ucuncs89 <ucuncs89@gmail.com>
2020-04-12 16:17:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/src/PHPTelebot.php';
 $bot = new PHPTelebot('1086380132:AAFkV2Bbjp1lOqoQSRoMawMwDdHGNKnTqZU', '@InfoMonitorBot');
 
 // Simple answer
-$bot->cmd('*', 'Hi, human! I am a bot.');
+$bot->cmd('/start', 'Hi, human! I am a bot.');
 
 // Simple echo command
 $bot->cmd('/echo|/say', function ($text) {
@@ -17,21 +17,35 @@ $bot->cmd('/echo|/say', function ($text) {
 });
 
 // Simple whoami command
-$bot->cmd('/whoami', function () {
+$bot->cmd('/info', function () {
     // Get message properties
+    // File json yang akan dibaca (full path file)
+$file = "https://api.kawalcorona.com/indonesia/";
+
+// Mendapatkan file json
+$anggota = file_get_contents($file);
+
+// Mendecode anggota.json
+$data = json_decode($anggota, true);
+
+// Membaca data array menggunakan foreach
+    //echo $data[0]['sembuh']. "<br>";
+    //echo $data[0]['positif']. "<br>";
+    //echo $data[0]['name']. "<br>";
+    
     $message = Bot::message();
     $name = $message['from']['first_name'];
     $userId = $message['from']['id'];
-    $text = 'You are <b>'.$name.'</b> and your ID is <code>'.$userId.'</code>';
+    $text = 'Info <b>'.$data[0]['name'].'</b> <br> Positif = '.$data[0]['positif'].'</br>';
     $options = [
         'parse_mode' => 'html',
         'reply' => true,
-    ];
+    ]; */
 
     return Bot::sendMessage($text, $options);
 });
 
-// slice text by space
+/* slice text by space
 $bot->cmd('/split', function ($one, $two, $three) {
     $text = "First word: $one\n";
     $text .= "Second word: $two\n";
@@ -58,7 +72,7 @@ $bot->cmd('/keyboard', function () {
     ];
 
     return Bot::sendMessage('Inline keyboard', $options);
-});
+}); */
 
 // custom regex
 $bot->regex('/\/number ([0-9]+)/i', function ($matches) {
