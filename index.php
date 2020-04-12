afb4d08a10f4ad3a6a8d0afb22b4df4be906e66b
ucuncs89 <ucuncs89@gmail.com>
2020-04-12 16:44:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $bot->cmd('/info', function () {
     $anggota = file_get_contents($file);
     $data = json_decode($anggota, true);
 
-    $text = 'Info di <b>'.$data[0]['name'].'</b> Data Positif <code>'.$data[0]['positif'].'</code> Sembuh <code>'.$data[0]['sembuh'].'</code> Meninggal<code>'.$data[0]['meninggal'].'</code>';
+    $text = 'Info di <b>'.$data[0]['name'].'</b><br> Data Positif <code>'.$data[0]['positif'].'</code><br> Sembuh <code>'.$data[0]['sembuh'].'</code><br> Meninggal <code>'.$data[0]['meninggal'].'</code>';
     $options = [
         'parse_mode' => 'html',
         'reply' => true,
